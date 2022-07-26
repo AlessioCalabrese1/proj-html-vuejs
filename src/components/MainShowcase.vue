195f18585052e00eb9fc9704cb05d427514fbddc
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-26 23:36:13 +0200

    MainShowcase: slider

--- src/components/MainShowcase.vue
+++ src/components/MainShowcase.vue
@@ -1,129 +1,73 @@
 <template>
   <section class="position-relative">
-    <div class="mainshowcase-slider">
-        <img src="../assets/img/rev-slider-main-home-img-03.jpg" alt="slider">
-        <img src="../assets/img/rev-slider-main-home-img-02.png" alt="slider" class="d-none">
-    </div>
-
-    <div class="mainshowcase-body">
-        <div>
-            <div class="row">
-                <div class="col-1">
-                    <i class="fa-solid fa-arrow-left-long"></i>
-                </div>
-
-                <div class="col-1"></div>
-
-                <div class="col-4">
-                    <div class="mainshowcae-title">
-                        Our Team<span>.</span>
-                    </div>
-                    <div class="mainshowcae-info my-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolores? Debitis odio veritatis ducimus natus porro blanditiis.
-                    </div>
-                    <div>
-                        <button class="more me-3">
-                            <span class="btn-text py-2 px-3">
-                                Read More
-                            </span>
-                            
-                            <span class="mx-2">
-                                <i class="fa-solid fa-arrow-right-long"></i>
-                            </span>
-                        </button>
-
-                        <button class="purchase">
-                            <span class="btn-text py-2 px-3">
-                                Purchase
-                            </span>
-                            
-                            <span class="mx-2">
-                                <i class="fa-solid fa-arrow-right-long"></i>
-                            </span>
-                        </button>
-                    </div>
-                </div>
-
-                <div class="col-5"></div>
-
-                <div class="col-1 text-end">
-                    <i class="fa-solid fa-arrow-right-long"></i>
-                </div>
-            </div>
-        </div>
+    <MainShowcaseItem v-for="(item, index) in showcaseItems" :key="index" 
+    :showcaseItem="item" 
+    :visibleItem="visibleItem"
+    :direction="direction"
+    :index="index" />
+    <div class="btn-container d-flex justify-content-between">
+        <i class="fa-solid fa-arrow-left-long" @click="goLeft()"></i>
+        <i class="fa-solid fa-arrow-right-long text-end" @click="goRight()"></i>
     </div>
   </section>
 </template>
 
 <script>
+import MainShowcaseItem from "./MainShowcaseItem.vue";
 export default {
+    data: function(){
+        return{
+            showcaseItems: [
+                {
+                    imgInBackground: "rev-slider-main-home-img-03.jpg",
+                    title: "Our Team",
+                    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolores? Debitis odio veritatis ducimus natus porro blanditiis.",
+                    btnMore: true,
+                    btnPurchase: true,
+                },
+                {
+                    imgInBackground: "rev-slider-main-home-img-02.png",
+                    title: "Test",
+                    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolores? Debitis odio veritatis ducimus natus porro blanditiis.",
+                    btnMore: true,
+                    btnPurchase: false,
+                },
+            ],
+            visibleItem: 0,
+            direction: "left",
+        }
+    },
+    methods: {
+        goRight(){
+            if (this.visibleItem == this.showcaseItems.length-1) {
+                this.visibleItem = 0;
+            }else{
+                this.visibleItem++;
+            }
+        },
 
+        goLeft(){
+            if (this.visibleItem == 0) {
+                this.visibleItem = this.showcaseItems.length-1;
+            }else{
+                this.visibleItem--;
+            }
+        }
+    },
+
+    components: { MainShowcaseItem }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../styles/variables.scss";
 
-    .mainshowcase-slider{
-        height: 900px;
-        overflow: hidden;
-        img{
-            position: relative;
-            top: -70px;
-            left: 0;
-        }
-    }
-
-    .mainshowcase-body{
-        width: $widthElement;
-        margin: 0 auto;
-        position: absolute;
-        left: 50%;
-        bottom: 220px;
-        transform: translate(-50%, 0);
-    }
-
-    .mainshowcase-body > div{
-        width: 100%;
-    }
-
-    .col-1{
-        padding-top: 75px;
-    }
-    .mainshowcae-title{
-        font-size: 75px;
-        font-weight: bold;
-        span{
-            color: $textColorHover;
-        }
-    }
-
-    .mainshowcae-info{
-        color: $textInfoColor;
-    }
-
-    .more, .purchase{
-        background-color: white;
-        border-radius: 0;
-        padding: 15px;
-        border: 0;
-    }
-
-    .purchase{
-        background-color: $textColorHover;
-        .btn-text{
-            color: white;
-            border-right: 1px solid white;
-        }
-        i{
-            color: white;
-        }
-    }
-
-    .btn-text{
-        border-right: 1px solid black;
-        text-transform: uppercase;
-        font-weight: 600;
-        font-size: 14px;
-    }
+.btn-container{
+    width: $widthElement;
+    margin: 0 auto;
+    position: absolute;
+    top: 65%;
+    left: 50%;
+    transform: translate(-50%, 0);
+}
 </style>
---
+++ src/components/MainShowcaseItem.vue
@@ -0,0 +1,214 @@
+<template>
+    <transition :name="direction">
+        <div v-show="visibleItem === index" class="position-relative carousel_item">
+            <div class="mainshowcase-slider">
+                <img :src="require(`../assets/img/${showcaseItem.imgInBackground}`)" alt="slider">
+            </div>
+
+            <div class="mainshowcase-body">
+                <div class="row">
+
+                    <div class="col-2"></div>
+
+                    <div class="col-4">
+                        <div class="mainshowcase-title">
+                            {{ showcaseItem.title }}<span>.</span>
+                        </div>
+                        <div class="mainshowcase-info my-3">
+                            {{ showcaseItem.info }}
+                        </div>
+
+                        <div class="btn-container">
+                            <button class="more me-3" v-if="showcaseItem.btnMore">
+                                <span class="btn-text py-2 px-3">
+                                    Read More
+                                </span>
+
+                                <span class="mx-2">
+                                    <i class="fa-solid fa-arrow-right-long"></i>
+                                </span>
+                            </button>
+
+                            <button class="purchase" v-if="showcaseItem.btnPurchase">
+                                <span class="btn-text py-2 px-3">
+                                    Purchase
+                                </span>
+
+                                <span class="mx-2">
+                                    <i class="fa-solid fa-arrow-right-long"></i>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="col-6"></div>
+                </div>
+            </div>
+        </div>
+    </transition>
+
+
+</template>
+
+<script>
+export default {
+    props: {
+        showcaseItem: {
+            type: Object,
+            require: true
+        },
+
+        index: {
+            type: Number,
+            require: true
+        },
+
+        visibleItem: {
+            type: Number,
+            require: true
+        },
+
+        direction:{
+            type: String,
+            require: true
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+.mainshowcase-slider {
+    height: 900px;
+    overflow: hidden;
+
+    img {
+        position: relative;
+        top: -70px;
+        left: 0;
+    }
+}
+
+.mainshowcase-body {
+    width: $widthElement;
+    margin: 0 auto;
+    position: absolute;
+    left: 50%;
+    bottom: 220px;
+    transform: translate(-50%, 0);
+}
+
+.mainshowcase-body>div {
+    width: 100%;
+}
+
+.col-1 {
+    padding-top: 75px;
+}
+
+.mainshowcase-title {
+    font-size: 75px;
+    font-weight: bold;
+
+    span {
+        color: $textColorHover;
+    }
+}
+
+.mainshowcase-info {
+    color: $textInfoColor;
+}
+
+.more,
+.purchase {
+    background-color: white;
+    border-radius: 0;
+    padding: 15px;
+    border: 0;
+}
+
+.purchase {
+    background-color: $textColorHover;
+
+    .btn-text {
+        color: white;
+        border-right: 1px solid white;
+    }
+
+    i {
+        color: white;
+    }
+}
+
+.btn-text {
+    border-right: 1px solid black;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 14px;
+}
+
+.carousel_item {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+.left-enter-active {
+    animation: leftInAnimation 0.4s ease-in-out;
+}
+
+.left-leave-active {
+    animation: leftOutAnimation 0.4s ease-in-out;
+}
+
+@keyframes leftInAnimation {
+    from {
+        transform: translateX(100%);
+    }
+
+    to {
+        transform: translateX(0%);
+    }
+}
+
+@keyframes leftOutAnimation {
+    from {
+        transform: translateX(0%);
+    }
+
+    to {
+        transform: translateX(-100%);
+    }
+}
+
+.right-enter-active {
+    animation: rightInAnimation 0.4s ease-in-out;
+}
+
+.right-leave-active {
+    animation: rightOutAnimation 0.4s ease-in-out;
+}
+
+@keyframes rightInAnimation {
+    from {
+        transform: translateX(-100%);
+    }
+
+    to {
+        transform: translateX(0%);
+    }
+}
+
+@keyframes rightOutAnimation {
+    from {
+        transform: translateX(0%);
+    }
+
+    to {
+        transform: translateX(+100%);
+    }
+}
+</style>
