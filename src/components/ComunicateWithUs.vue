b71c6067b80f8a59883b94f57785d18e0bb80fc6
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-28 11:23:46 +0200

    ComunicateWithUs: form setup

--- src/components/ComunicateWithUs.vue
+++ src/components/ComunicateWithUs.vue
@@ -3,35 +3,18 @@
     <div>
         <div class="comunicate-container">
             <div class="row">
-                <div class="col-6 position-relative">
-                    <img src="../assets/img/h1-contact-rev-01.png" alt="maps" class="img-fluid">
-                    <div class="pointer-structure new-york">
-                        <img src="../assets/img/h1-contact-rev-02.png" alt="pointer" class="pointer">
-                        <div class="info-box">
-                            <p class="title">New York Coaching</p>
-                            <p>Read More</p>
-                        </div>
-                    </div>
-
-                    <div class="pointer-structure london">
-                        <img src="../assets/img/h1-contact-rev-02.png" alt="pointer" class="pointer">
-                        <div class="info-box">
-                            <p class="title">London Coaching</p>
-                            <p class="more">Read More</p>
-                        </div>
-                    </div>
-
-                    <div class="pointer-structure melbourne">
-                        <img src="../assets/img/h1-contact-rev-02.png" alt="pointer" class="pointer">
-                        <div class="info-box">
-                            <p class="title">Melbourne Coaching</p>
-                            <p class="more">Read More</p>
-                        </div>
-                    </div>
-                </div>
+                <Maps />
 
                 <div class="col-6">
-
+                    <div>Contact Us<span>.</span></div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div class="line"></div>
+                    <form>
+                        <input type="text" placeholder="Your Name" class="name">
+                        <input type="email" placeholder="Your Email" class="email">
+                        <input type="text" placeholder="Write a message..." class="w-100 text">
+                        <input type="submit">
+                    </form>
                 </div>
             </div>
         </div>
@@ -40,8 +23,9 @@
 </template>
 
 <script>
+import Maps from './Maps.vue';
 export default {
-
+    components: { Maps }
 }
 </script>
 
@@ -58,50 +42,16 @@ section > div{
     margin: 0 auto;
 }
 
-.info-box{
-    width: 250px;
-    background-color: white;
-    padding: 20px;
-    display: none;
-    box-shadow: 0px 5px 5px black;
-    p{
-        margin: 0;
-    }
-}
-.new-york{
-    position: absolute;
-    top: 105px;
-    left: 145px;
-}
-.london{
-    position: absolute;
-    top: 65px;
-    left: 300px;
-}
-.melbourne{
-    position: absolute;
-    top: 220px;
-    left: 480px;
-}
-.title{
-    font-weight: bold;
-    font-size: 20px;
-}
-.more{
-    font-weight: bold;
-    text-transform: uppercase;
-    font-size: 12px;
+.line{
+    border: 1px solid $textColorHover;
+    width: 10%;
 }
 
-
-
-.pointer:hover{
-    cursor: pointer;
+.name{
+    margin-right: 15px;
+    width: calc(50% - 7.5px);
 }
-.pointer:hover + .info-box{
-    display: block;
-}
-.info-box:hover{
-    display: block;
+.email{
+    width: calc(50% - 7.5px);
 }
 </style>
