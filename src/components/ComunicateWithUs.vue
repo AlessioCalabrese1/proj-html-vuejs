fd9b5b12a2ecf84ceb9e71636b30700f0be3d7d2
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-07-28 10:49:06 +0200

    CumunicateWithUs: map implementation

--- src/components/ComunicateWithUs.vue
+++ src/components/ComunicateWithUs.vue
@@ -3,10 +3,30 @@
     <div>
         <div class="comunicate-container">
             <div class="row">
-                <div class="col-6">
+                <div class="col-6 position-relative">
                     <img src="../assets/img/h1-contact-rev-01.png" alt="maps" class="img-fluid">
-                    <div class="pointer-structure">
+                    <div class="pointer-structure new-york">
                         <img src="../assets/img/h1-contact-rev-02.png" alt="pointer" class="pointer">
+                        <div class="info-box">
+                            <p class="title">New York Coaching</p>
+                            <p>Read More</p>
+                        </div>
+                    </div>
+
+                    <div class="pointer-structure london">
+                        <img src="../assets/img/h1-contact-rev-02.png" alt="pointer" class="pointer">
+                        <div class="info-box">
+                            <p class="title">London Coaching</p>
+                            <p class="more">Read More</p>
+                        </div>
+                    </div>
+
+                    <div class="pointer-structure melbourne">
+                        <img src="../assets/img/h1-contact-rev-02.png" alt="pointer" class="pointer">
+                        <div class="info-box">
+                            <p class="title">Melbourne Coaching</p>
+                            <p class="more">Read More</p>
+                        </div>
                     </div>
                 </div>
 
@@ -36,5 +56,52 @@ section > div{
 .comunicate-container{
     width: 70%;
     margin: 0 auto;
+}
+
+.info-box{
+    width: 250px;
+    background-color: white;
+    padding: 20px;
+    display: none;
+    box-shadow: 0px 5px 5px black;
+    p{
+        margin: 0;
+    }
+}
+.new-york{
+    position: absolute;
+    top: 105px;
+    left: 145px;
+}
+.london{
+    position: absolute;
+    top: 65px;
+    left: 300px;
+}
+.melbourne{
+    position: absolute;
+    top: 220px;
+    left: 480px;
+}
+.title{
+    font-weight: bold;
+    font-size: 20px;
+}
+.more{
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 12px;
+}
+
+
+
+.pointer:hover{
+    cursor: pointer;
+}
+.pointer:hover + .info-box{
+    display: block;
+}
+.info-box:hover{
+    display: block;
 }
 </style>
